7f3905b8c352c47111ad0a3955a017cb9fd40dec
UmBarril <74427600+UmBarril@users.noreply.github.com>
2025-04-18 19:29:13 -0300

    removing old unused autocmd

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -95,38 +95,6 @@ vim.cmd [[
         autocmd BufRead,BufNewFile package.json nnoremap <buffer> <silent> gx :call PackageJsonGx()<cr>
     augroup END
 
-    " make gx work in plug manager (https://doriankarter.com/customize-vims-gx-mapping/)
-    function! PlugGx()
-        let l:line = getline('.')
-        let l:sha  = matchstr(l:line, '^  \X*\zs\x\{7,9}\ze ')
-
-        if (&filetype ==# 'vim-plug')
-            " inside vim plug splits such as :PlugStatus
-            let l:name = empty(l:sha)
-                        \ ? matchstr(l:line, '^[-x+] \zs[^:]\+\ze:')
-                        \ : getline(search('^- .*:$', 'bn'))[2:-2]
-        else
-            " in .vimrc.bundles
-            let l:name = matchlist(l:line, '\v/([A-Za-z0-9\-_\.]+)')[1]
-        endif
-
-        let l:uri  = get(get(g:plugs, l:name, {}), 'uri', '')
-        if l:uri !~? 'github.com'
-            return
-        endif
-        let l:repo = matchstr(l:uri, '[^:/]*/'.l:name)
-        let l:url  = empty(l:sha)
-                    \ ? 'https://github.com/'.l:repo
-                    \ : printf('https://github.com/%s/commit/%s', l:repo, l:sha)
-        call netrw#BrowseX(l:url, 0)
-    endfunction
-
-    augroup PlugGxGroup
-        autocmd!
-        autocmd BufRead,BufNewFile plugins.lua nnoremap <buffer> <silent> gx :call PlugGx()<cr>
-        autocmd FileType vim-plug nnoremap <buffer> <silent> gx :call PlugGx()<cr>
-    augroup END
-
     " TODO: make gx work on obsidian.md markdown files
     function! ObsidianMD() abort
         let l:line = getline('.')
